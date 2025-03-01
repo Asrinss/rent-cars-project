fe2376fdda663298949ab462b53e446866894a33
Asrin Halic <a.halic@hotmail.com>
2025-03-01 17:12:38 +0300

    github-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Statik export için gerekli
+  images: {
+    unoptimized: true, // GitHub Pages için image optimizasyonu kapat
+  },
+  basePath: "/rent-cars-project", // GitHub repo adıyla aynı olmalı!
+  assetPrefix: "/rent-cars-project/",
 };
 
 export default nextConfig;
